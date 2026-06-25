cdaa167ca4d6cf585271f21016c6f10959ae00fc
CrisProgrammer <103343319+CrisProgrammer@users.noreply.github.com>
2026-06-25 15:37:31 -0400

    Add files via upload

---
+++ target/debug/deps/macroquad_macro-c9f0318aab4d342f.d
@@ -0,0 +1,5 @@
+C:\Users\xorio\Documents\cris\rust\proyecto\target\debug\deps\macroquad_macro-c9f0318aab4d342f.d: C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\macroquad_macro-0.1.8\src\lib.rs
+
+C:\Users\xorio\Documents\cris\rust\proyecto\target\debug\deps\macroquad_macro-c9f0318aab4d342f.dll: C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\macroquad_macro-0.1.8\src\lib.rs
+
+C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\macroquad_macro-0.1.8\src\lib.rs:
---
+++ target/debug/deps/quad_rand-367b0accbd194c77.d
@@ -0,0 +1,8 @@
+C:\Users\xorio\Documents\cris\rust\proyecto\target\debug\deps\quad_rand-367b0accbd194c77.d: C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quad-rand-0.2.3\src\lib.rs C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quad-rand-0.2.3\src\fy.rs
+
+C:\Users\xorio\Documents\cris\rust\proyecto\target\debug\deps\libquad_rand-367b0accbd194c77.rlib: C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quad-rand-0.2.3\src\lib.rs C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quad-rand-0.2.3\src\fy.rs
+
+C:\Users\xorio\Documents\cris\rust\proyecto\target\debug\deps\libquad_rand-367b0accbd194c77.rmeta: C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quad-rand-0.2.3\src\lib.rs C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quad-rand-0.2.3\src\fy.rs
+
+C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quad-rand-0.2.3\src\lib.rs:
+C:\Users\xorio\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\quad-rand-0.2.3\src\fy.rs:
