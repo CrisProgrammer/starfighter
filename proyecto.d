899239c0f436ddf6f99c10e0f952af6c9fdb6771
CrisProgrammer <103343319+CrisProgrammer@users.noreply.github.com>
2026-06-25 15:22:02 -0400

    Add files via upload

---
+++ proyecto.d
@@ -0,0 +1,6 @@
+C:\Users\xorio\Documents\cris\rust\proyecto\target\debug\deps\proyecto.d: src\main.rs src\entidades.rs
+
+C:\Users\xorio\Documents\cris\rust\proyecto\target\debug\deps\proyecto.exe: src\main.rs src\entidades.rs
+
+src\main.rs:
+src\entidades.rs:
